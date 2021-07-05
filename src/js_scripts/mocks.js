93cb248c8c79d85b10702b1df49f285a378c6499
flo <floriandavid.lange@web.de>
2021-07-05 19:30:09 +0200

    added mock for db status object

---
+++ src/js_scripts/mocks.js
@@ -0,0 +1,15 @@
+export let db_status = {
+    spectra_count: {
+        sfg: 2430,
+        lt: 526,
+        ir: 3,
+        raman: 2,
+        uv: 8
+    },
+    last_upload: "2021-07-03",
+    c_sfg_count: {
+        regular: 1158,
+        boknis: 843,
+        gasex: 265
+    }
+}
